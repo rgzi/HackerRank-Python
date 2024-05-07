fe8141835aee1b009a208b1aeb8d41fbb8ae1d70
rgzi <161358764+rgzi@users.noreply.github.com>
2024-05-07 21:28:41 +0400

    Create reverse-and-swap.py

---
+++ reverse-and-swap.py
@@ -0,0 +1,18 @@
+def reverse_words_order_and_swap_cases(sentence):
+    # Write your code here
+    reverse_word = list()
+    
+    for word in sentence:
+        if word.isupper():
+            word = word.lower() 
+            reverse_word.append(word)
+        else:
+            word = word.upper()
+            reverse_word.append(word)  
+
+    reverse_word = "".join(reverse_word)
+    reverse_word = reverse_word.split(" ")
+    reverse_word = reverse_word[::-1]
+    for word in reverse_word:
+        print(word, end=" ")
+reverse_words_order_and_swap_cases("aWESOME is cODING")
